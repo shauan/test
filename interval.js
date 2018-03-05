6a56da6b5760d5f2e1188989b084224ee65ea1e6
shauan <shauan@shauan.local>
2018-03-05 14:54:58 -0500

    push files

---
+++ interval.js
@@ -0,0 +1,56 @@
+// console.log('working')
+// const h1 = document.createElement('h1');
+// h1.textContent = 'Test';
+// h1.classList.add('hello');
+// h1.id = 'hello-world';
+// document.body.appendChild(h1);
+
+
+
+// const pTags = document.querySelectorAll('li');
+//
+// function changeBackground() {
+//   var rand = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+//   var randDefault = rand + 500;
+//
+//   for(var i = 0; i < pTags.length; i++) {
+//     setInterval(function() {
+//       pTags[0].style.background = '#52489C';
+//     }, rand)
+//     setInterval(function() {
+//       pTags[1].style.background = '#59C3C3';
+//     }, rand)
+//     setInterval(function() {
+//       pTags[2].style.background = '#CAD2C5';
+//     }, rand)
+//     setInterval(function() {
+//       pTags[3].style.background = '#84A98C';
+//     }, rand)
+//     setInterval(function() {
+//       pTags[4].style.background = '#D2F898';
+//     }, rand)
+//     setInterval(function() {
+//       pTags[5].style.background = '#F786AA';
+//     }, rand);
+//
+//         setInterval(function() {
+//           pTags[0].style.background = '#fff';
+//         }, randDefault)
+//         setInterval(function() {
+//           pTags[1].style.background = '#fff';
+//         }, randDefault)
+//         setInterval(function() {
+//           pTags[2].style.background = '#fff';
+//         }, randDefault)
+//         setInterval(function() {
+//           pTags[3].style.background = '#fff';
+//         }, randDefault)
+//         setInterval(function() {
+//           pTags[4].style.background = '#fff';
+//         }, randDefault)
+//         setInterval(function() {
+//           pTags[5].style.background = '#fff';
+//         }, randDefault)
+//   }
+// }
+// changeBackground()
